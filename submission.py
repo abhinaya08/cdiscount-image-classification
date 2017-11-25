54367b187d2d180f8b79f5b181395386647050f5
lx10077 <lixiang10077@qq.com>
2017-11-25 20:56:05 +0800

    not debug yet

--- submission.py
+++ submission.py
@@ -1,5 +1,5 @@
 import os
-used_gpu = '2'
+used_gpu = '1'
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = used_gpu
 
@@ -7,10 +7,10 @@ from utils import *
 from se_inception_v3 import *
 
 
-cfg = {"image_size": (3,150,150),
+cfg = {"image_size": (3, 180, 180),
        "num_classes": 5270,
        "num_test": 1768182,
-       "saved_model": "epoch-14-acc-0.63.pkl",
+       "saved_model": "epoch-5-acc-0.67.pth",
        "test_bson_path": '/data/lixiang/test.bson',
        "batch_size": 256,
        "data_worker": 4}
@@ -22,7 +22,16 @@ def submission(cfg):
         net = net.cuda()
 
     print("*-------Begin Loading Saved Models!------*")
-    net.load_pretrained_model('saved_models/' + cfg["saved_model"])
+    pretrain_state_dict = get_state_dict('saved_models/' + cfg["saved_model"])
+    state_dict = net.state_dict()
+    keys = list(state_dict.keys())
+    for key in keys:
+        try:
+            state_dict[key] = pretrain_state_dict["module."+key]
+        except KeyError:
+            print("KeyError: {} dosen't lie in pretrain state dict".format(key))
+            continue
+    net.load_state_dict(state_dict)
     net.eval()
 
     print("*----------Begin Loading Data!-----------*")
@@ -36,12 +45,7 @@ def submission(cfg):
     save_pd = pd.DataFrame()
     save_pd["category_id"] = net.predict(test_loader)
     save_pd["_id"] = data_frame.index
-    save_pd.to_csv("submission2.csv", columns=["_id","category_id"], index=False, sep=",")
-
-    #save_pd = pd.DataFrame()
-    #save_pd["category_id"] = net.predict(test_loader)
-    #save_pd = data_frame.index
-    #save_pd.index.name = "_id"
+    save_pd.to_csv("submission3.csv", columns=["_id","category_id"], index=False, sep=",")
     print("*------------End Submission!------------*")
 
 
